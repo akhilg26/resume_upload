4c97dac862fd3debac73473bf11956b72af40209
akhilg26 <akhil.giddaluri@gmail.com>
2026-07-21 15:32:25 -0400

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Header
 from app.routes import auth
 from app.routes import upload
+from app.routes import create_match
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +9,7 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(upload.router)
+app.include_router(create_match.router)
 
 
 
--- backend/app/routes/create_match.py
+++ backend/app/routes/create_match.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 from app.dependencies import verify_token
 import os
 import psycopg2
-from models.match import Match
+from app.models.match import Match
+import numpy as np
+import ast
 
 load_dotenv()
 router = APIRouter()
@@ -16,13 +18,25 @@ def match(match: Match, user_id: int = Depends(verify_token)):
 
     cursor.execute('SELECT embedding FROM resumes WHERE id = %s AND user_id = %s', (match.resume_id, user_id))
     result = cursor.fetchone()
-    if result is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Resume not found; most likely that user_id does not match with resume'
-        )
+    cursor.execute('SELECT embedding FROM job_descriptions WHERE id = %s AND user_id = %s', (match.job_id, user_id))
+    result1 = cursor.fetchone()
+    if result is None or result1 is None:
+            raise HTTPException(
+                status_code=404,
+                detail='Job/resume not found'
+            )
 
-    cursor.execute('INSERT INTO matches (resume_id, job_id, score) VALUES (%s, %s, %s) RETURNING id', (match.resume_id, match.job_id, 100))
+    resume_embedding_string = result[0]
+    job_embedding_string = result1[0]
+    resume_embedding = np.array(ast.literal_eval(resume_embedding_string))
+    job_embedding = np.array(ast.literal_eval(job_embedding_string))
+    dot_product = np.dot(resume_embedding, job_embedding)
+    magnitude = np.linalg.norm(resume_embedding) * np.linalg.norm(job_embedding)
+    cos_similarity = dot_product / magnitude
+    score = round(cos_similarity * 100, 2)
+
+
+    cursor.execute('INSERT INTO matches (resume_id, job_id, score) VALUES (%s, %s, %s) RETURNING id', (match.resume_id, match.job_id, score))
     result = cursor.fetchone()
     conn.commit()
     return {'message': 'Success',
